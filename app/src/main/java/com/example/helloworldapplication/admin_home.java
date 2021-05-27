cac8fde75a3b99cc09f1d2f95f7a28b67bec8f9a
irfan <ali.irfan42@outlook.com>
2021-05-27 17:14:53 +0530

    Changes by Asif..

--- app/src/main/java/com/example/helloworldapplication/admin_home.java
+++ app/src/main/java/com/example/helloworldapplication/admin_home.java
@@ -3,6 +3,7 @@ package com.example.helloworldapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class admin_home extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class admin_home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+        Toast.makeText(admin_home.this, "Incorrect Email Or Password !!", Toast.LENGTH_SHORT).show();
+
     }
 }
